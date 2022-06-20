af38dcd2324837554e6edaf8e158cc88ceda34a2
Zodiarkaic <Jessie.bwilson88@yahoo.com>
2022-06-20 00:37:23 -0500

    Actual Fixed Mob Movement

--- MobSurvivor/Assets/Scripts/MobMovement.cs
+++ MobSurvivor/Assets/Scripts/MobMovement.cs
@@ -7,38 +7,37 @@ public class MobMovement : MonoBehaviour
     public Transform player;
     public float moveSpeed = 0.2f;
     private Rigidbody2D rb;
-    private Vector2 movement; 
+    private Vector2 direction;
+    //private Vector2 movement;
+
 
 
     // Start is called before the first frame update
     void Start()
     {
         rb = this.GetComponent<Rigidbody2D>();
-
-    
-    
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 direction = player.position - transform.position;
-        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-        rb.rotation = angle;
-        //direction.Normalize();
-        //direction.Normalize slowed mobs down as they got closer to Player Character
-        movement = direction;
+        direction = player.position - transform.position;
+        //float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        //rb.rotation = angle;
+        direction.Normalize();
     }
+
     private void FixedUpdate() 
+        //FixedUpdate is called every Frame per Framerate
     {
-        moveCharacter(movement);
-    
-    
-    
+        //moveCharacter(movement);
+        moveCharacter(direction);
     }
 
     void moveCharacter(Vector2 direction)
     {
+        Physics2D.SyncTransforms();
         rb.MovePosition((Vector2)transform.position + (direction * moveSpeed * Time.deltaTime));
+        //rb.MovePosition(Vector2 position);
     }
 }
