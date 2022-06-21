8d75bc481d95e98daa6953cc2496fe8d3fa7305e
Zodiarkaic <Jessie.bwilson88@yahoo.com>
2022-06-20 22:09:25 -0500

    Merge Error Fix

---
+++ MobSurvivor/Assets/Scripts/HPbar.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HPbar : MonoBehaviour
+{
+    public Slider slider;
+
+    // Setting values of slider to Max and current value of Health
+    public void SetMaxHealth(int MaxHealth)
+    {
+        slider.maxValue = MaxHealth;
+        slider.value = MaxHealth;
+
+    }
+    // Setting the slider on Healthbar to current Health
+    public void SetCurrentHealth(int currentHealth)
+    {
+        slider.value -= currentHealth;
+        
+    }
+
+}
--- MobSurvivor/Assets/Scripts/PlayerHealth.cs
+++ MobSurvivor/Assets/Scripts/PlayerHealth.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class PlayerHealth : MonoBehaviour
 {
+    public int healthLevel = 10;
+    public int maxHealth;
+    public int currentHealth;
+    public HPbar healthbar;
+
     // Start is called before the first frame update
+    // Setting maxHealth to maxhealth based on healthlevel while setting current health at the start of the level to max health and letting healthbar reflect that
     void Start()
     {
+        maxHealth = SetMaxHealthFromHealthLevel();
+        currentHealth = maxHealth;
+        healthbar.SetMaxHealth(maxHealth);
         
+    }
+    // Arbitrary number play around with it to find a good value
+    private int SetMaxHealthFromHealthLevel()
+    {
+        maxHealth = healthLevel * 10;
+        return maxHealth;
+    }
+    // Sets current health based on damage taken and reflects that damage on healthbar
+    public void TakeDamage(int damage)
+    {
+        currentHealth = currentHealth - damage;
+        healthbar.SetCurrentHealth(currentHealth);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
