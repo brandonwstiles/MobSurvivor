19e5b196865fbf9cd795792e0940663225da7a6f
Brandon Stiles <39569543+brandonwstiles@users.noreply.github.com>
2022-06-20 22:07:57 -0500

    Revert "Added Bullet and Gun Mechanics"
    This reverts commit 52380bb5c33e8ae9b703fa50ba2d2ae256462df9.

--- MobSurvivor/Assets/Scripts/GunControls.cs
+++ MobSurvivor/Assets/Scripts/GunControls.cs
@@ -4,31 +4,9 @@ using UnityEngine;
 
 public class GunControls : MonoBehaviour
 {
-    public Transform SpawnLocation;
-    public GameObject bullet;
-    public float cooldown = .5f;
-
-    private void Start()
-    {
-        StartCoroutine(WeaponCooldown(cooldown));
-    }
     void Update()
     {
         Vector2 mousePos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0));
-        transform.right = mousePos - (Vector2)transform.position; 
-    }
-
-    void FireWeapon(GameObject bullet)
-    {
-        Instantiate(bullet, SpawnLocation.position, SpawnLocation.rotation);
-    }
-
-    IEnumerator WeaponCooldown(float seconds)
-    {
-        while(true)
-        {
-                FireWeapon(bullet);
-                yield return new WaitForSeconds(seconds);
-        }
+        transform.right = mousePos - (Vector2)transform.position;
     }
 }
